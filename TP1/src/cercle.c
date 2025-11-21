59094b43d5a9a35f65bb554bbfb81da09e0e6030
YazidBbz <157983875+YazidBbz@users.noreply.github.com>
2025-11-21 10:11:35 +0100

    Calculate area and perimeter of a circle

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -3,8 +3,13 @@
 
 int main() {
 double pi = 3.1416;
-double r = 6;  
-    printf("L'air du cercle = %f", pi*(r*r));
-   printf("Le périmètre du cercle = %f", pi*(r+r));
+double r = 6; 
+double perimetre = 2*pi*r;
+double aire = pi*r*r;
+    
+    printf("L'air du cercle = %f", aire);
+   printf("Le périmètre du cercle = %f", perimetre);
+    
 }
+
 
